c57ad5e2fa8012e07524548914d9bb65e8b42087
alwinsDen <jsmadesimple@gmail.com>
2023-05-07 12:41:30 +0530

    feat(editor_registry): Added preset functionality for object selection.

--- src/EditorEngine/editorComponents/SetupControls/Presets/SectionThree/index.tsx
+++ src/EditorEngine/editorComponents/SetupControls/Presets/SectionThree/index.tsx
@@ -1,16 +1,91 @@
 // @ts-nocheck
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MaterialControlsProps } from "../../../../PropsControls";
-import { updatePresets } from "../../../../../redux/savedConfigs";
+import {
+  massUpdatePresets,
+  updatePresets,
+  updateUnUsedObjects,
+} from "../../../../../redux/savedConfigs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const SectionThree = () => {
-  //this here is the material redux control
   const { materialArray }: MaterialControlsProps = useSelector(
     (state: any) => state.materialControl
   );
-  const { presets } = useSelector((state) => state.savedConfigs);
+  const { presets, unUsedObjects } = useSelector((state) => state.savedConfigs);
+  const [currentPreset, setCurrentPreset] = useState("");
+  const [toggleAdd, setToggleAdd] = useState(false);
   const dispatch = useDispatch();
+  useEffect(() => {
+    let materialNameList = [];
+    materialArray.map((vls) => {
+      materialNameList.push(vls.name);
+    });
+    dispatch(updateUnUsedObjects(materialNameList));
+  }, [materialArray]);
+
+  const MaterialSelection = ({ unSelectedObjects, current }) => {
+    const [updatedList, setUpdatedList] = useState([]);
+    return (
+      <div style={{ border: "1px solid black" }}>
+        <h3>{current}</h3>
+        {unSelectedObjects.map((objectConfig) => {
+          return (
+            <div style={{ display: "flex", gap: "5px", margin: "3px" }}>
+              <input
+                type={"checkbox"}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setUpdatedList((upState) => {
+                      return [...upState, objectConfig];
+                    });
+                  } else {
+                    setUpdatedList((upState) => {
+                      let retArray = upState.filter((v) => v != objectConfig);
+                      return [...retArray];
+                    });
+                  }
+                }}
+              />
+              <p>{objectConfig}</p>
+            </div>
+          );
+        })}
+        <button
+          onClick={() => {
+            //selected the objects of the preset
+            let presetObject = presets.map((prVakl) => {
+              if (prVakl.name == current) {
+                return {
+                  ...prVakl,
+                  materialList: [...prVakl.materialList, ...updatedList],
+                  visibility: [
+                    ...prVakl.visibility,
+                    ...Array(updatedList.length).fill(true),
+                  ],
+                };
+              }
+              return prVakl;
+            });
+            dispatch(
+              updateUnUsedObjects(
+                unSelectedObjects.filter(
+                  (cuvls) => !updatedList.includes(cuvls)
+                )
+              )
+            );
+            dispatch(massUpdatePresets(presetObject));
+            setToggleAdd((state) => !state);
+          }}
+        >
+          Save
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div>
       <div
@@ -29,12 +104,21 @@ const SectionThree = () => {
               materialList: [],
               visibility: [],
             };
+            setCurrentPreset(name);
             dispatch(updatePresets(innerObject));
+            setToggleAdd((state) => !state);
           }}
         >
           Add+
         </button>
       </div>
+      ;{/*here is the material selection section*/}
+      {toggleAdd && (
+        <MaterialSelection
+          unSelectedObjects={unUsedObjects}
+          current={currentPreset}
+        />
+      )}
       <div>
         {presets.map((preset) => {
           return (
@@ -45,14 +129,47 @@ const SectionThree = () => {
                 padding: "4px",
               }}
             >
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
                 <p>{preset.name}</p>
-                <button>+</button>
+                <button
+                  onClick={() => {
+                    setCurrentPreset(preset.name);
+                    setToggleAdd((state) => !state);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+              <div>
+                {preset.materialList.map((matVal) => {
+                  return (
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        margin: "5px 0",
+                        alignItems: "center",
+                      }}
+                    >
+                      <p>{matVal}</p>
+                      <FontAwesomeIcon
+                        icon={faEye}
+                        style={{ fontSize: "14px", color: "darkgrey" }}
+                      />
+                    </div>
+                  );
+                })}
               </div>
             </div>
           );
         })}
       </div>
+      ;
     </div>
   );
 };
--- src/redux/savedConfigs.ts
+++ src/redux/savedConfigs.ts
@@ -4,12 +4,20 @@ const savedConfigs = createSlice({
   name: "savedConfigs",
   initialState: <any>{
     presets: [],
+    unUsedObjects: [],
   },
   reducers: {
     updatePresets: (state, action) => {
       state.presets = [...state.presets, action.payload];
     },
+    updateUnUsedObjects: (state, action) => {
+      state.unUsedObjects = action.payload;
+    },
+    massUpdatePresets: (state, action) => {
+      state.presets = action.payload;
+    },
   },
 });
-export const { updatePresets } = savedConfigs.actions;
+export const { updatePresets, updateUnUsedObjects, massUpdatePresets } =
+  savedConfigs.actions;
 export default savedConfigs.reducer;
