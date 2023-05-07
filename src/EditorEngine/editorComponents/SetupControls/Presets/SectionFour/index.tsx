4d7f8024732ae3a3534d3125f73e9e744f3c790e
alwinsDen <jsmadesimple@gmail.com>
2023-05-07 19:16:33 +0530

    feat(editor_registry): Added material review support

--- src/EditorEngine/editorComponents/SetupControls/Presets/SectionFour/index.tsx
+++ src/EditorEngine/editorComponents/SetupControls/Presets/SectionFour/index.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {updateAmbientLight, updateDirLight,} from "../../../../../redux/materialControl";
-
+import * as THREE from "three";
+import {AmbientLight, DirectionalLight, Mesh, PerspectiveCamera, Scene, WebGLRenderer} from "three";
 //image imports
 import ObjectPng from "../../../../../assets/pngs/objectLogo.gif";
 
@@ -18,7 +19,36 @@ const SectionFour = () => {
     (state) => state.materialControl.directionalLight
   );
   const dispatch = useDispatch();
+  // image preview
+  const createMaterialThumbnail = (material, size = 128) => {
+    // Create a WebGLRenderer
+    const renderer = new WebGLRenderer();
+    renderer.setSize(size, size);
 
+    // Create a scene and a camera
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
+    camera.position.z = 2;
+
+    const ambientLight = new AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
+    const directionalLight = new DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+
+    // Create a mesh with the input material
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const mesh = new Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Render the scene
+    renderer.render(scene, camera);
+
+    // Get the data URL from the renderer and return it
+    const dataURL = renderer.domElement.toDataURL("image/png");
+    return dataURL;
+  };
   return (
     <div>
       <div className={"lightcontrols"}>
@@ -90,6 +120,24 @@ const SectionFour = () => {
       </div>
       <div className={"productMeshes"}>
         <h3>Product Materials</h3>
+        {materialArray.map((mesh, index) => (
+          <div
+            key={index}
+            className="material-preview"
+            style={{ margin: "5px" }}
+          >
+            <img
+              src={createMaterialThumbnail(mesh.material)}
+              alt={mesh.name}
+              style={{
+                width: "128px",
+                height: "128px",
+                borderRadius: "10px",
+              }}
+            />
+            <p>{mesh.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
