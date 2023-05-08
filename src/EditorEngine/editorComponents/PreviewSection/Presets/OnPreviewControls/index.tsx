bccb1a2b7f5fe8d663a2e6484d898139547bd54d
alwinsDen <jsmadesimple@gmail.com>
2023-05-08 22:08:19 +0530

    feat(editor_registry): Added draggable UI components.

--- src/EditorEngine/editorComponents/PreviewSection/Presets/OnPreviewControls/index.tsx
+++ src/EditorEngine/editorComponents/PreviewSection/Presets/OnPreviewControls/index.tsx
@@ -14,11 +14,88 @@ import RImage2 from "../../../../../assets/svgs/OnPreviewAssets/reload.svg";
 import RImage3 from "../../../../../assets/svgs/OnPreviewAssets/3d (1) 1.svg";
 import RImage4 from "../../../../../assets/svgs/OnPreviewAssets/settings.svg";
 
+//dragables
+import Draggable from "react-draggable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateAmbientLight,
+  updateDirLight,
+} from "../../../../../redux/materialControl";
+
 const OnPreviewControls = () => {
   const preImages = [Image1, Image2, Image3, Image4, Image5, Image6];
   const preRImages = [RImage1, RImage2, RImage3, RImage4];
+  // redux states
+  const ambientLight = useSelector(
+    (state: any) => state.materialControl.ambientLight
+  );
+  const directionalLight = useSelector(
+    (state: any) => state.materialControl.directionalLight
+  );
+  const dispatch = useDispatch();
   return (
     <div className={"OnPreviewControlsDiv"}>
+      {/*  here is the drag test*/}
+      <Draggable
+        onStart={() => {
+          console.log("drag drag");
+        }}
+        onStop={() => {
+          console.log("drag stop");
+        }}
+        handle=".dragHeader"
+      >
+        <div className={"draggableComp"}>
+          <div className={"dragHeader"}>
+            <FontAwesomeIcon icon={faBars} style={{ cursor: "grabbing" }} />
+            <p>Lighting</p>
+            <FontAwesomeIcon icon={faXmarkCircle} />
+          </div>
+
+          {/*here is the main lighting component*/}
+          <div className={"dragControls"}>
+            <div>
+              <p>Ambient Occlusion</p>
+              <input
+                type="range"
+                min={1}
+                max={10}
+                defaultValue={ambientLight * 10}
+                onChange={(e) => {
+                  dispatch(updateAmbientLight(e.target.value));
+                }}
+              />
+            </div>
+            <input
+              type={"text"}
+              className={"inputDisplay"}
+              placeholder={(ambientLight / 10).toString()}
+            />
+          </div>
+          <div className={"dragControls"}>
+            <div>
+              <p>Directional light</p>
+              <input
+                type="range"
+                min={1}
+                max={100}
+                defaultValue={directionalLight}
+                onChange={(e) => {
+                  dispatch(updateDirLight(e.target.value));
+                }}
+              />
+            </div>
+            <input
+              type={"text"}
+              className={"inputDisplay"}
+              placeholder={directionalLight.toString()}
+            />
+          </div>
+        </div>
+      </Draggable>
+
       {/*these are the left side controls*/}
       <div className={"prevButtonControl"}>
         {preImages.map((img: any, index) => {
