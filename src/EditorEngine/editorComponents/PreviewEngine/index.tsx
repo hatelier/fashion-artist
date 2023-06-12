2b8b2a9a173b10b37f9ad88257350fadc2649d08
alwinsDen <jsmadesimple@gmail.com>
2023-06-12 14:27:21 +0530

    feat(editor_registry): Toggle meshes.

--- src/EditorEngine/editorComponents/PreviewEngine/index.tsx
+++ src/EditorEngine/editorComponents/PreviewEngine/index.tsx
@@ -14,6 +14,78 @@ import { MeshPhysicalMaterial, TextureLoader } from "three";
 import * as THREE from "three";
 
 const PreviewEngine = () => {
+  return (
+    <div>
+      <div className="preview-page">
+        <div className="preview-block">
+          <div className="preview-main">
+            <img
+              src={require("../../../assets/pngs/mx-logo-dark.png")}
+              alt=""
+              className="preview-mtum-logo"
+            />
+            <div className="preview-view-ar">View in AR</div>
+            <SideMenu />
+            <div className="preview-area" style={{ width: "100%" }}>
+              <ModelPreview />
+            </div>
+            <div className="preview-options">
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-1.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-2.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-3.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-4.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-5.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a href="">
+                  <img
+                    src={require("../../../assets/pngs/preview-6.png")}
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const SideMenu = () => {
   //dispatches
   const dispatch = useDispatch();
   // useStates
@@ -142,169 +214,122 @@ const PreviewEngine = () => {
     }
   }, [materialList]);
   return (
-    <div>
-      <div className="preview-page">
-        <div className="preview-block">
-          <div className="preview-main">
-            <img
-              src={require("../../../assets/pngs/mx-logo-dark.png")}
-              alt=""
-              className="preview-mtum-logo"
-            />
-            <div className="preview-view-ar">View in AR</div>
-            <div className="preview-sidemenu">
-              {presetData &&
-                presetData.map((presData, index) => {
-                  return (
-                    <div
-                      className="dropdown"
-                      style={{
-                        position: "relative",
-                        background: "#eeeeee",
-                        borderRadius: "20px",
-                        width: "300px",
-                      }}
-                    >
-                      <button className="preview-sidemenu-label">
-                        <span>{presData.name}</span>
-                        <img
-                          src={require("../../../assets/pngs/plus-white.png")}
-                          alt=""
-                          onClick={() => {
-                            setSelectedTab(index);
-                          }}
-                        />
-                      </button>
+    <div className="preview-sidemenu">
+      {presetData &&
+        presetData.map((presData, index) => {
+          return (
+            <div
+              className="dropdown"
+              style={{
+                position: "relative",
+                background: "#eeeeee",
+                borderRadius: "20px",
+                width: "300px",
+              }}
+            >
+              <button className="preview-sidemenu-label">
+                <span>{presData.name}</span>
+                <img
+                  src={require("../../../assets/pngs/plus-white.png")}
+                  alt=""
+                  onClick={() => {
+                    setSelectedTab(index);
+                  }}
+                />
+              </button>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
+                {selectedTab === index &&
+                  presData.materialList.map((mateList, inx) => {
+                    return (
                       <div
                         style={{
+                          margin: "5px",
+                          width: "90px",
+                          // background: "red",
                           display: "flex",
-                          flexWrap: "wrap",
-                          justifyContent: "center",
+                          flexDirection: "column",
+                          alignItems: "center",
+                          padding: "10px 5px",
                         }}
                       >
-                        {selectedTab === index &&
-                          presData.materialList.map((mateList, inx) => {
-                            return (
-                              <div
-                                style={{
-                                  margin: "5px",
-                                  width: "90px",
-                                  // background: "red",
-                                  display: "flex",
-                                  flexDirection: "column",
-                                  alignItems: "center",
-                                  padding: "10px 5px",
-                                }}
-                              >
-                                <FontAwesomeIcon
-                                  icon={faCube}
-                                  style={{
-                                    fontSize: "50px",
-                                  }}
-                                />
-                                <p
-                                  style={{
-                                    fontSize: "12px",
-                                  }}
-                                >
-                                  {mateList.substring(0, 13)}
-                                </p>
+                        <input
+                          type={"checkbox"}
+                          style={{ marginBottom: "5px" }}
+                          defaultChecked={presData.visibility[inx]}
+                          onChange={(e) => {
+                            if (e.target.checked) {
+                              materialList.map((modMaterial, modIndex) => {
+                                console.log(modMaterial.name);
+                                if (modMaterial.name === mateList) {
+                                  modMaterial.visible = true;
+                                }
+                              });
+                            } else {
+                              materialList.map((modMaterial, modIndex) => {
+                                console.log(modMaterial.name);
+                                if (modMaterial.name === mateList) {
+                                  modMaterial.visible = false;
+                                }
+                              });
+                            }
+                          }}
+                        />
+                        <FontAwesomeIcon
+                          icon={faCube}
+                          style={{
+                            fontSize: "50px",
+                          }}
+                        />
+                        <p
+                          style={{
+                            fontSize: "12px",
+                          }}
+                        >
+                          {mateList.substring(0, 13)}
+                        </p>
 
-                                {/*here is the material list*/}
-                                {configData && (
+                        {/*here is the material list*/}
+                        {configData && (
+                          <div
+                            style={{
+                              marginTop: "5px",
+                              display: "flex",
+                              gap: "10px",
+                              flexWrap: "wrap",
+                              justifyContent: "center",
+                            }}
+                          >
+                            {configData[mateList] &&
+                              configData[mateList].list.map((name) => {
+                                return (
                                   <div
                                     style={{
-                                      marginTop: "5px",
-                                      display: "flex",
-                                      gap: "10px",
-                                      flexWrap: "wrap",
-                                      justifyContent: "center",
+                                      background:
+                                        configData[mateList].selected === name
+                                          ? "green"
+                                          : "red",
+                                      width: "10px",
+                                      height: "10px",
                                     }}
-                                  >
-                                    {configData[mateList] &&
-                                      configData[mateList].list.map((name) => {
-                                        return (
-                                          <div
-                                            style={{
-                                              background:
-                                                configData[mateList]
-                                                  .selected === name
-                                                  ? "green"
-                                                  : "red",
-                                              width: "10px",
-                                              height: "10px",
-                                            }}
-                                          ></div>
-                                        );
-                                      })}
-                                  </div>
-                                )}
-                              </div>
-                            );
-                          })}
+                                  ></div>
+                                );
+                              })}
+                          </div>
+                        )}
                       </div>
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="preview-area" style={{ width: "100%" }}>
-              <ModelPreview />
-            </div>
-            <div className="preview-options">
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-1.png")}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-2.png")}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-3.png")}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-4.png")}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-5.png")}
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img
-                    src={require("../../../assets/pngs/preview-6.png")}
-                    alt=""
-                  />
-                </a>
+                    );
+                  })}
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          );
+        })}
     </div>
   );
 };
-
 export default PreviewEngine;
