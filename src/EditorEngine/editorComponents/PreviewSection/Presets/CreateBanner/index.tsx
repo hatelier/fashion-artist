bac770893532bbb41a86653292d2fbae16fc4e66
alwinsDen <jsmadesimple@gmail.com>
2023-06-12 16:22:33 +0530

    feat(editor_registry): Minor URL changes.

--- src/EditorEngine/editorComponents/PreviewSection/Presets/CreateBanner/index.tsx
+++ src/EditorEngine/editorComponents/PreviewSection/Presets/CreateBanner/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./index.scss";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const CreateBanner = () => {
+  const { id } = useParams();
+  const { userID, projectID } = useSelector(
+    (state: any) => state.accountManagement
+  );
+  const baseReactUrl = window.location.origin.toString();
   return (
     <div className={"createBanner"}>
       <p> {"< Create"} </p>
-      <button>Publish</button>
+      <button
+        onClick={() => {
+          window.alert(`${baseReactUrl}/view/${userID}/${projectID}/${id}`);
+        }}
+      >
+        Publish
+      </button>
     </div>
   );
 };
