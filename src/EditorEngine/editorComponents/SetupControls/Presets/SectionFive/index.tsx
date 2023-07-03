fa6dc5842daf71a3d133a90e4a1b13069d578012
alwinsDen <jsmadesimple@gmail.com>
2023-07-03 16:19:06 +0530

    feat(editor_engine): Added 3D space APIs and UI improves

--- src/EditorEngine/editorComponents/SetupControls/Presets/SectionFive/index.tsx
+++ src/EditorEngine/editorComponents/SetupControls/Presets/SectionFive/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { updateEnableComments } from "../../../../../redux/commentsRedux";
+import AddConfig from "../../../../../assets/svgs/AddConfig.svg";
+import "./index.scss";
 const SectionFive = () => {
   const dispatch = useDispatch();
   return (
-    <button
-      onClick={() => {
-        dispatch(updateEnableComments());
+    <div
+      style={{
+        margin: "0 20px",
       }}
     >
-      Vieew commnts
-    </button>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          margin: "36px 0 6px 0",
+          justifyContent: "space-between",
+        }}
+      >
+        <p className={"sectionFiveTitle"}>Collaboration Feedback</p>
+        <img
+          src={AddConfig}
+          style={{ width: "21.35px", filter: "invert(.5)" }}
+        />
+        <img src={AddConfig} style={{ width: "21.35px" }} />
+      </div>
+    </div>
   );
 };
 export default SectionFive;
