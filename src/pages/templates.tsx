7d38b6e5cd39af9c17d59a50ea000f26bc8a52c5
PaarthAgarwal <paarth.agarwal.mec20@itbhu.ac.in>
2023-07-10 14:03:24 +0530

    added template page and fix

--- src/pages/templates.tsx
+++ src/pages/templates.tsx
@@ -1,7 +1,73 @@
+import { Header } from '../components/header';
+import { Sidenav } from '../components/sidenav';
+import Card from '../components/Card';
+
 export const Templates = () => {
     return (
-        <div>
-            <h1>Templates</h1>
+        <div className='home-container'>
+  <section >
+  <Header />
+    <div className='content'>
+      <Sidenav />
+         <div className='main'>
+          <div className="main-header">
+           <div className='text'>
+             <div className='dashboard'>
+               <div className="analytics-header">
+                  <span>Templates</span>
+               </div>
+             </div>
+           </div>
+           </div>
+           <div className="subscription">
+             <div className="templates-block">
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+              <div className="card-name">dummy</div>
+          </Card>
+          <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+               <Card>
+               <div className="card-img-container">
+                <img className="card-img" src="dummy" alt="Preview" />
+               </div>
+               <div className="card-name">dummy</div>
+               </Card>
+             </div>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  </div>
+);
 };
