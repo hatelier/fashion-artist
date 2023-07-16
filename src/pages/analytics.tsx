80cab2a7abce920039895102acb60b9307d574fd
PaarthAgarwal <paarth.agarwal.mec20@itbhu.ac.in>
2023-07-16 11:56:11 +0530

    analytics page and teams page

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -171,154 +171,189 @@ export const Analytics = () => {
                     </div>
                 </div> 
                 )}
-    <section >
-    <Header />
-      <div className='content'>
-        <Sidenav />
-           <div className='main'>
-           <div className="main-header">
-             <div className='text'>
-               <div className='dashboard'>
-                 <div className="analytics-header">
-                    <span>Analytics</span>
-                    <button onClick={()=>{}} className="analytics-download-button">Download</button>
-                 </div>
-               </div>
-             </div>
-             </div>
-             <div className="subscription">
-               <div className="analytics-block">
-                 <div className="analytics-report-block">
-                   <div className="analytics-report">
-                     <div className="analytics-report-header">
-                        <div className="analytics-report-header-text">Report</div>
-                        <div className="analytics-report-header-buttons">
-                            <button onClick={()=>{}}>Product Report</button>
-                            <button onClick={()=>{}}>Reset</button>
-                        </div>
-                        <div>
-                        <label htmlFor="range">By Date Range</label>
-                          <select value={range} onChange={Report}>
-                            <option value="">Day</option>
-                            <option value="">Week</option>
-                            <option value="">Month</option>
-                          </select>
-                        </div>
-                        <div>
-                        <label htmlFor="teams">By Teams</label>
-                          <select value={range}>
-                            <option value="">Day</option>
-                            <option value="">Week</option>
-                            <option value="">Month</option>
-                          </select>
-                        </div>
-                        <div>
-                        <label htmlFor="showroom">By Showroom</label>
-                          <select value={range}>
-                            <option value="">Day</option>
-                            <option value="">Week</option>
-                            <option value="">Month</option>
-                          </select>
-                        </div>
-                        <div>
-                          <label htmlFor="product">By Product</label>
-                          <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-                          <button onClick={handleSearch}>Search</button>
-                          {products && (
-                            <div>
-                              <img src={products.location} alt={products.productName}  style={{ marginTop: '10px', marginLeft: '5px' }} height={350} width={280}/>
-                              <h3>{products.productName}</h3>
-                            </div>
-                          )}
-                        </div>
-                     </div>
-                   </div>
-                   <div className="analytics-report-pagination">
+      <section>
+      <Header />
+        <div className='content'>
+          <Sidenav />
+            <div className='main height-100vh'>
+            <div className="main-header">
+              <div className='text'>
+                <div className='dashboard'>
+                  <div className="analytics-header">
+                      <span>Analytics</span>
+                      <button onClick={()=>{}} className="analytics-download-button">Download</button>
+                  </div>
+                </div>
+              </div>
+              </div>
+              <div className="analytics-block-main">
+                <div className="analytics-block">
+                  <div className="analytics-report-block">
+                    <div className="analytics-report">
+                      <div className="analytics-report-header">
+                          <div className="analytics-report-header-text">Report</div>
+                          <div className="analytics-report-header-buttons">
+                              <button onClick={()=>{}}>Product Report</button>
+                              <button onClick={()=>{}} style={{"backgroundColor":"transparent"}}>Reset</button>
+                          </div>
+                          </div>
 
-                   </div>
-                 </div>
-                 <div className="analytics-secondary-block">
-                  <div className="analytics-secondary-top">
-                    <div className="analytics-3d-views">
-                    <div className="period-selection">
-                      <select value={selectedPeriod} onChange={handlePeriodChange}>
-                        <option value="day">Day</option>
-                        <option value="week">Week</option>
-                        <option value="month">Month</option>
-                      </select>
-                    </div>
-                      <LineChart width={400} height={350} data={threeDViewCount}>
-                        <XAxis dataKey="date" 
-                        tickFormatter={(dateStr) => {
-                          const date = new Date(dateStr);
-                          const options = { month: 'long', day: 'numeric' } as const;
-                          return date.toLocaleString('en-US', options);  
-                        }}
-                        />
-                        <CartesianGrid stroke='#ccc' />
-                        <Tooltip />
-                        <Legend />
-                        <Line type='monotone' dataKey='count' stroke='blue' name='Views'/>
-                        <YAxis />
-                      </LineChart>
-                    </div>
-                    <div className="analytics-stats">
-                        <div className="analytics-user-engagement">
-                            <div className="analytics-stats-heading">All User Engagement</div>
-                            <div className='analytics-stats-main'>
-                                <img src={require('../assets/pngs/analytics-user.png')} alt="" />
-                                <span className='analytics-stats-number'>{engagementPercentage} %</span>
-                            </div>
+                        <div className="analytics-selections">
+                          <div className='analytics-selections-element'>
+                            <label htmlFor="range">By Date Range</label>
+                              <select value={range} onChange={Report}>
+                                <option value="">Day</option>
+                                <option value="">Week</option>
+                                <option value="">Month</option>
+                              </select>
+                          </div>
+                          <div className='analytics-selections-element'>
+                          <label htmlFor="teams">By Teams</label>
+                            <select value={range}>
+                              <option value="">Day</option>
+                              <option value="">Week</option>
+                              <option value="">Month</option>
+                            </select>
+                          </div>
+                          <div className='analytics-selections-element'>
+                          <label htmlFor="showroom">By Showroom</label>
+                            <select value={range}>
+                              <option value="">Day</option>
+                              <option value="">Week</option>
+                              <option value="">Month</option>
+                            </select>
+                          </div>
+                          <div className='analytics-selections-element'>
+                            <label htmlFor="product">By Product</label>
+                            <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                          </div>
                         </div>
-                        <div className="analytics-average-interaction">
-                            <div className="analytics-stats-heading">No. of Average Interaction</div>
-                            <div>
-                                <img src="" alt="" />
-                                <span className='analytics-stats-number'>{avgInteraction}</span>
-                            </div>
+
+                        <div className="analytics-report-3">
+                          <div className="analytics-report-item">
+                            <input type='checkbox'/>
+                            <img src={require('../assets/pngs/rectangle.png')} alt="back" />
+                            <p>Product Name</p>
+                          </div>
+
+                          <div className="analytics-report-item">
+                            <input type='checkbox'/>
+                            <img src={require('../assets/pngs/rectangle.png')} alt="" />
+                            <p>Product Name</p> 
+                          </div>
+
+                          <div className="analytics-report-item">
+                            <input type='checkbox'/>
+                            <img src={require('../assets/pngs/rectangle.png')} alt="back" />
+                            <p>Product Name</p>
+                          </div>
+                      
+
                         </div>
+                        <div className="analytics-report-pagination">
+                            <img src={require('../assets/pngs/back.png')} alt="" className='sidenav-img'/>
+                            <div className='analytics-pagination-count'> 
+                              <div>1</div>
+                              <div>2</div>
+                              <div>3</div>
+                              <div>4</div>
+                              <div>5</div>
+                            </div>
+                            <img src={require('../assets/pngs/back.png')} alt="" className='sidenav-img' style={{"transform": "rotate(180deg)"}}/>
+                    </div>
                     </div>
                   </div>
-                  <div className="analytics-secondary-bottom">
-                    <div className="analytics-most-viewed">
-                    {mostViewedProduct ? (
-                      <img src={mostViewedProduct} style={{ marginTop: '10px', marginLeft: '5px' }} height={350} width={280} alt="Most Viewed Product" />
-                      ) : (
-                      <p>No most viewed product available</p>
-                      )}
+                  <div className="analytics-secondary-block">
+                    <div className="analytics-secondary-top">
+                      <div className="analytics-3d-views">
+                      <div className="period-selection">
+                        <p>Total 3D view</p>
+                        <select value={selectedPeriod} onChange={handlePeriodChange}>
+                          <option value="day">Day</option>
+                          <option value="week">Week</option>
+                          <option value="month">Month</option>
+                        </select>
+                      </div>
+                      <div className='analytics-linechart-container'>
+                        {/* <LineChart width={250} height={350} data={threeDViewCount} className='analytics-linechart'>
+                          <XAxis dataKey="date" 
+                          tickFormatter={(dateStr) => {
+                            const date = new Date(dateStr);
+                            const options = { month: 'long', day: 'numeric' } as const;
+                            return date.toLocaleString('en-US', options);  
+                          }}
+                          /> */}
+                          {/* width={400} height={350} */}
+                          {/* <CartesianGrid stroke='#ccc' />
+                          <Tooltip />
+                          <Legend />
+                          <Line type='monotone' dataKey='count' stroke='blue' name='Views'/>
+                          <YAxis />
+                        </LineChart> */}
+                        {/* <img src={require('../assets/pngs/graph3dview.png')} alt="back" className='analytics-linechart' /> */}
+                        </div>
+                      </div>
+                      <div className="analytics-stats">
+                          <div className="analytics-user-engagement">
+                              <div className="analytics-stats-heading">All User Engagement</div>
+                              <div className='analytics-stats-main'>
+                                  <img src={require('../assets/pngs/analytics-img.png')} alt="" className='sidenav-img'/>
+                                  <span className='analytics-stats-number'>{engagementPercentage} %</span>
+                              </div>
+                          </div>
+                          <div className="analytics-average-interaction">
+                              <div className="analytics-stats-heading">No. of Average Interaction</div>
+                              <div className='analytics-stats-main'>
+                                  <img src="" alt="" />
+                                  <span className='analytics-stats-number'>{avgInteraction}</span>
+                              </div>
+                          </div>
+                      </div>
                     </div>
-                    <div className="analytics-ar-views">
-                    <div className="period-selection">
-                      <select value={selectedPeriod} onChange={handleARPeriodChange}>
-                        <option value="day">Day</option>
-                        <option value="week">Week</option>
-                        <option value="month">Month</option>
-                      </select>
-                    </div>
-                    <LineChart width={400} height={370} data={arViewCount}>
-                        <XAxis dataKey="date" 
-                        tickFormatter={(dateStr) => {
-                          const date = new Date(dateStr);
-                          const options = { month: 'long', day: 'numeric' } as const;
-                          return date.toLocaleString('en-US', options);  
-                        }}
-                        />
-                        <CartesianGrid stroke='#ccc' />
-                        <Tooltip />
-                        <Legend />
-                        <Line type='monotone' dataKey='count' name='Views' stroke='blue'/>
-                        <YAxis />
-                        <Legend wrapperStyle={{ display: 'none' }} />
-                      </LineChart>
+                    <div className="analytics-secondary-bottom">
+                      <div className="analytics-most-viewed">
+                      <div>Most viewed product</div>
+                      {/* {mostViewedProduct && (
+                        <img src={mostViewedProduct} style={{ marginTop: '10px', marginLeft: '5px' }} height={350} width={280} alt="Most Viewed Product" />
+                        )} */}
+                        <div className="analytics-most-viewed-img">
+                        <img src={require('../assets/pngs/mostviewproduct.png')} alt="back" className='analytics-linechart' style={{"height":"100%"}}/>
+                        <div className='analytics-download-button analytics-product-report-btn'>Product Report</div>
+                        </div>
+                      </div>
+                      <div className="analytics-ar-views">
+                      <div className="period-selection">
+                        <p>Total AR views</p>
+                        <select value={selectedPeriod} onChange={handleARPeriodChange}>
+                          <option value="day">Day</option>
+                          <option value="week">Week</option>
+                          <option value="month">Month</option>
+                        </select>
+                      </div>
+                      {/* <LineChart width={250} height={350} data={arViewCount}>
+                          <XAxis dataKey="date" 
+                          tickFormatter={(dateStr) => {
+                            const date = new Date(dateStr);
+                            const options = { month: 'long', day: 'numeric' } as const;
+                            return date.toLocaleString('en-US', options);  
+                          }}
+                          />
+                          <CartesianGrid stroke='#ccc' />
+                          <Tooltip />
+                          <Legend />
+                          <Line type='monotone' dataKey='count' name='Views' stroke='blue'/>
+                          <YAxis />
+                          <Legend wrapperStyle={{ display: 'none' }} />
+                        </LineChart> */}
+                        <img src={require('../assets/pngs/totalARview.png')} alt="back" className='analytics-linechart' />
+                      </div>
                     </div>
                   </div>
-                 </div>
-               </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <TokenVerification />
+        </section>
     </div>
 );
 };
